7652924c23eb68e88d4cfc78aed4fe7d56fa3bd2
yangwon9616 <yangwon9616@gmail.com>
2021-08-26 10:58:26 +0900

    My First Commit

---
+++ DownUpGame/CreditViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreditViewController.swift
+//  DownUpGame
+//
+//  Created by 조양원 on 2021/07/04.
+//
+
+import UIKit
+
+class CreditViewController: UIViewController {
+
+    
+    @IBAction func touchUpDissmissButton (_ sender: UIButton) {
+        
+    dismiss(animated: true, completion: nil)
+    }
+}
